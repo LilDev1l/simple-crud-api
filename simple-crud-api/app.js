4b42c08ae70f4ee5d9a089dc17fba1f861d73ae2
LilDev1l <lildev1l.ya@gmail.com>
2021-11-28 22:02:54 +0300

    refactor: change structure of the project

--- simple-crud-api/app.js
+++ simple-crud-api/app.js
@@ -1,2 +1,35 @@
+const http = require('http');
+const {personController} = require('./src/controllers/person.controller');
+const customErrors = require('./src/errors');
+
 require('dotenv').config();
-require('./src/server').startServer();
+
+const server = http.createServer();
+
+server.on('request', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    try {
+        if (/^\/person/.test(req.url)) {
+            await personController(req, res);
+        } else {
+            res.statusCode = 404;
+            res.end(JSON.stringify({message: 'Resource that you requested doesn\'t exist'}));
+        }
+    } catch (err) {
+        if (err instanceof customErrors.NotFoundPersonError ||
+            err instanceof customErrors.InvalidDataInRequestError) {
+            res.statusCode = err.statusCode;
+            res.end(JSON.stringify({message: err.message}));
+        } else {
+            res.statusCode = 500;
+            res.end(JSON.stringify({message: 'Server error'}));
+        }
+    }
+});
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}...`);
+});
+
+module.exports = server;
